3f2bf499c08f012785b3dc62d3f60cbb2197ad51
Jefferson Carley <jeff.carley@gmail.com>
2011-10-30 12:00:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpikeFileupload::Application.config.secret_token = '19b63eb4c7625fa1b4d473cdf0a8b58c662e501fe2dcfcc52845fe683a98b026484087c447fc4ae57b878950b608ae0b092b6692c5c5f88383764d50b6279a29'
